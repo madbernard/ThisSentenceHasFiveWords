0ac9c5a1a350f0148ad89c3b55cea39e8230fa4d
Mad Bernard <madeline.a.bernard@gmail.com>
2018-02-04 14:56:55 -0800

    Make display function, display result on page

--- public/fivewords.js
+++ public/fivewords.js
@@ -1,15 +1,19 @@
 var seeColorsButton,
   textBody,
-  sentenceEnds = /\n|[.?!]['"]*\s+/;
+  sentenceEnds = /\n|[.?!]['"]*\s+/, // todo, not "mr. dr. etc"
+  wordBreaks = /\w[-–—]\w|\s+/,
+  sentenceInfo,
+  processedText;
 
 document.addEventListener('DOMContentLoaded', function(event) {
   init();
 });
 
 function init (){
-  // provision elements
+  // identify elements
   seeColorsButton = document.getElementById('seeColorsButton');
   textBody = document.getElementById('textBody');
+  displayArea = document.getElementById('display');
 
   // set listeners
   seeColorsButton.onclick = processText;
@@ -20,6 +24,25 @@ function init (){
 function processText (){
   var text = textBody.value;
   var sentenceArray = text.split(sentenceEnds);
-  console.log('sentenceArray you did it', sentenceArray);
+
+  sentenceInfo = sentenceArray.map(function loadObject (sentence) {
+    var length = sentence.split(wordBreaks).length;
+    return {
+      sentence: sentence,
+      length: length,
+      // todo before make html check length bounds
+      html: '<span class="l-' + length + '">' + sentence + '</span>'
+    };
+  });
+  console.log('sentenceInfo you did it', sentenceInfo);
+  displayText();
 }
 
+function displayText (){
+  // todo: collect choices of spacing, join with those
+  processedText = sentenceInfo.map(function collectHTML (obj){
+    return obj.html;
+  }).join('');
+  console.log('processedText you did it', processedText);
+  displayArea.innerHTML = processedText;
+}
