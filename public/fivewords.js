cc8b62431d0058929ed0790dd18ce1df5502d255
Mad Bernard <madeline.a.bernard@gmail.com>
2018-02-04 13:58:08 -0800

    Initial focus; drafting main function

--- public/fivewords.js
+++ public/fivewords.js
@@ -1,7 +1,25 @@
+var seeColorsButton,
+  textBody,
+  sentenceEnds = /\n|[.?!]['"]*\s+/;
+
 document.addEventListener('DOMContentLoaded', function(event) {
   init();
 });
 
 function init (){
-  console.log('Init you did it');
+  // provision elements
+  seeColorsButton = document.getElementById('seeColorsButton');
+  textBody = document.getElementById('textBody');
+
+  // set listeners
+  seeColorsButton.onclick = processText;
+
+  document.getElementById('fiveWordsTitle').focus();
 }
+
+function processText (){
+  var text = textBody.value;
+  var sentenceArray = text.split(sentenceEnds);
+  console.log('sentenceArray you did it', sentenceArray);
+}
+
