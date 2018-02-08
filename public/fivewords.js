69e0db35acf2e5026199242603371e61284b930d
Mad Bernard <madeline.a.bernard@gmail.com>
2018-02-07 22:56:32 -0800

    Spacing/highlighting of sentences looks nicer

--- public/fivewords.js
+++ public/fivewords.js
@@ -2,6 +2,7 @@ var seeColorsButton,
   textBody,
   sentenceEnds = /(\n|[.?!]+['"]*\s+)/, // todo, not "mr. dr. etc"
   wordBreaks = /\w[-–—]\w|\s+/,
+  endSpaces = /\s+$/,
   sentenceInfo,
   processedText;
 
@@ -27,37 +28,36 @@ function processText (){
 
   var sentenceArray = [];
 
+  // split with capture regex results in [text, capture, text, capture]
   for (var i = 0; i < wordsAndEndsArray.length; i = i + 2) {
+    // todo: check match against sentenceEnds, turn \n into <br>
     if (wordsAndEndsArray[i+1]) {
-      sentenceArray.push( wordsAndEndsArray[i] + wordsAndEndsArray[i+1] );
+      var sentenceWithEnd = wordsAndEndsArray[i] + wordsAndEndsArray[i+1];
+      sentenceArray.push(sentenceWithEnd.replace(endSpaces, ''));
     }
     else {
-      sentenceArray.push(wordsAndEndsArray[i]);
+      sentenceArray.push(wordsAndEndsArray[i].replace(endSpaces, ''));
     }
   }
 
   sentenceInfo = sentenceArray.map(function loadObject (sentence) {
-    // there's a space after each period, so subtract 1 "word"
-    var length = sentence.split(wordBreaks).length - 1;
-    if (length) {
-      return {
-        sentence: sentence,
-        length: length,
-        // todo before make html check length bounds
-        html: '<span class="l-' + length + '">' + sentence + '</span>'
-      };
-    }
+    var length = sentence.split(wordBreaks).length;
+    return {
+      sentence: sentence,
+      length: length,
+      // todo before make html check length bounds
+      html: '<span class="l-' + length + '">' + sentence + '</span>'
+    };
   });
   console.log('sentenceInfo you did it', sentenceInfo);
   displayText();
 }
 
 function displayText (){
-  // var joiner = ' '
-  // todo: collect choices of spacing, join with those
+  // &ensp; comes out to 8px (0.5 rem) which fits span negative margins
   processedText = sentenceInfo.map(function collectHTML (obj){
     return obj.html;
-  }).join('');
+  }).join('&ensp;');
   console.log('processedText you did it', processedText);
   displayArea.innerHTML = processedText;
 }
